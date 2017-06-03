622b4a0e9ac2a128fc67b51a312a74c049c86e79
Dustin Deus <deusdustin@gmail.com>
2017-06-03 12:50:57 +0200

    setup project

---
+++ .eslintrc.js
@@ -0,0 +1,29 @@
+module.exports = {
+  "env": {
+    "es6": true,
+    "node": true,
+    "mocha": true
+  },
+  "plugins": [],
+  "extends": [
+    "standard"
+  ],
+  "parserOptions": {
+    "sourceType": "module"
+  },
+  "globals": {
+    "Hemera": true,
+    "expect": true,
+    "HemeraTestsuite": true,
+    "HemeraUtil": true,
+    "Sinon": true,
+    "Code": true,
+    "UnauthorizedError": true,
+    "NodeVersion": true
+  },
+  // see http://eslint.org/docs/rules/
+  "rules": {
+    "indent": ["error", 2],
+    "quotes": ["error", "single"]
+  }
+};
